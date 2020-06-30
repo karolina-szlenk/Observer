4d79de83c328e6ca26444a299213756346914c7c
Karolina <karolina.szlenk@gmail.com>
2020-06-30 10:35:59 +0200

    code optimized

--- src/Observer.js
+++ src/Observer.js
@@ -1,29 +1,33 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Observer.css'
 
-const Observer = ({ children }) => {
+const useObserver = (options) => {
   const [visible, setVisible] = useState(false)
-  const ref = useRef(null)
+  const [ref, setRef] = useState(null)
 
   useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        console.log(entry)
-        setVisible(entry.isIntersecting)
-      },
-      {
-        root: null,
-        threshold: 0.3,
-        rootMargin: '0px',
-      }
-    )
-    if (ref.current) {
-      observer.observe(ref.current)
+    const observer = new IntersectionObserver(([entry]) => {
+      console.log(entry)
+      setVisible(entry.isIntersecting)
+    }, options)
+    if (ref) {
+      observer.observe(ref)
     }
-  }, [ref])
 
+    return () => {
+      if (ref) {
+        observer.unobserve(ref)
+      }
+    }
+  }, [ref, options])
+
+  return [setRef, visible]
+}
+
+const Observer = ({ children }) => {
+  const [setRef, visible] = useObserver({ threshold: 0.3 })
   return (
-    <div ref={ref} className={visible ? 'show' : 'hide'}>
+    <div ref={setRef} className={visible ? 'show' : 'hide'}>
       {children}
     </div>
   )
